e4f8c14f212669baf309bd664bc88cf3d4d53a76
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-11-03 18:34:31 +0530

    first commit

---
+++ assets/__federation_shared_react-dom-BPiK34Ci.js
@@ -0,0 +1,7 @@
+import { g as getDefaultExportFromCjs } from './_commonjsHelpers-B85MJLTf.js';
+import { r as requireReactDom } from './index-B3n7t_Dm.js';
+
+var reactDomExports = requireReactDom();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
+
+export { index as default };
